219acdb158cb830b149b2df6a2230f0cc61b1741
Prince Seth <156539518+Prince9369@users.noreply.github.com>
2025-05-26 23:59:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (91.47%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int helper(int sr,int sc,int er,int ec ,vector<vector<int>>& dp){
-        if(sr==er && sc==ec) return 1;//1 path to hai aisa
-        if(sr>er || sc>ec) return 0 ;
-        //jo cheej change ho rhi hai use memoize kro 
-        if(dp[sr][sc]!=-1) return dp[sr][sc];
-        return dp[sr][sc]=helper(sr,sc+1,er,ec,dp)+helper(sr+1,sc,er,ec,dp);//downways +rightways
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return helper(0,0,m-1,n-1,dp);
+        //tabulation bottum up dp
+        int dp[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0) dp[i][j]=1;
+                else dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
