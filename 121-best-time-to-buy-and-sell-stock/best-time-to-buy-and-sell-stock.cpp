ab52d767daf53923da43bb9b0d93b10d9c8619f8
Prince Seth <156539518+Prince9369@users.noreply.github.com>
2025-06-02 16:11:36 +0530

    Time: 7 ms (7.32%) | Memory: 97.5 MB (26.49%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
-   int dp[100005][2][2];
-
-int f(vector<int>& prices, int i, int k, bool on) {
-    if (i == prices.size() || k == 0) return 0;
-    if (dp[i][k][on] != -1) return dp[i][k][on];
-
-    int ans = f(prices, i + 1, k, on);
-    if (on) {
-        ans = max(ans, prices[i] + f(prices, i + 1, k - 1, false));
-    } else {
-        ans = max(ans, f(prices, i + 1, k, true) - prices[i]);
+    int maxProfit(vector<int>& prices) {
+        int minprice=INT_MAX,maxprice=0;
+        for(int price:prices){
+            minprice=min(minprice,price);
+            maxprice=max(maxprice,price-minprice);
+        }
+        return maxprice;
     }
-
-    return dp[i][k][on] = ans;
-}
-
-int maxProfit(vector<int>& prices) {
-    memset(dp, -1, sizeof(dp));
-    return f(prices, 0, 1, false);
-}
-
 };
