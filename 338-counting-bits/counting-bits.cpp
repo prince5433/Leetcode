728b753d1c01b486ad872aa9ed6b1a65bf0dfaee
Prince Seth <156539518+Prince9369@users.noreply.github.com>
2025-08-14 17:26:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (37.69%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> result;
-        for(int i=0;i<=n;i++){
-            result.push_back(__builtin_popcount(i));
+        // vector<int> result;
+        // for(int i=0;i<=n;i++){
+        //     result.push_back(__builtin_popcount(i));
+        // }
+        // return result;
+
+
+        //approach 2
+        vector<int> result(n+1);
+        result[0]=0;
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                result[i]=result[i/2]+1;
+            } else {
+                result[i]=result[i/2];
+            }
         }
         return result;
     }
