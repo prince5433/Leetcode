6617ffc1c864e6b3baa998f523f9dac9a8ebbb7f
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-05 19:20:43 +0530

    Time: 0 ms (100%), Space: 23.9 MB (97.53%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int count = 0;
-        for (int i = 0; i < flowerbed.size(); i++) {
-            // Check if the current plot is empty.
-            if (flowerbed[i] == 0) {
-                // Check if the left and right plots are empty.
-                bool emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
-                bool emptyRightPlot = (i == flowerbed.size() - 1) || (flowerbed[i + 1] == 0);
-                
-                // If both plots are empty, we can plant a flower here.
-                if (emptyLeftPlot && emptyRightPlot) {
-                    flowerbed[i] = 1;
-                    count++;
+        int m=flowerbed.size();
+        int cnt=0;
+        for(int i=0;i<m;i++){
+            if(flowerbed[i]==0){
+                bool left=(i==0)||(flowerbed[i-1]==0);
+                bool right=(i==m-1)||(flowerbed[i+1]==0);
+                if(left&&right){
+                    flowerbed[i]=1;
+                    cnt++;
                 }
+
             }
         }
-        return count >= n;
+        return cnt>=n;
     }
 };
