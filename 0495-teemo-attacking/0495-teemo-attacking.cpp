281bcaa9bf9925e350132ea492ff99d36e297d77
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-05 19:07:55 +0530

    Time: 3 ms (11.88%), Space: 29.8 MB (2.03%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int n=timeSeries.size();
+        int total=0;
+        for(int i=0;i<n-1;i++){
+            total+=min(timeSeries[i+1]-timeSeries[i],duration);
+        }
+        return total+duration;
+    }
+};
