916d98ecefba6f1903aa92e4e67f632ce06df690
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-05 19:21:15 +0530

    Time: 0 ms (100%), Space: 24.2 MB (41.02%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int m=flowerbed.size();
-        int cnt=0;
-        for(int i=0;i<m;i++){
-            if(flowerbed[i]==0){
-                bool left=(i==0)||(flowerbed[i-1]==0);
-                bool right=(i==m-1)||(flowerbed[i+1]==0);
-                if(left&&right){
-                    flowerbed[i]=1;
-                    cnt++;
-                }
+        int m = flowerbed.size();
+        int cnt = 0;
 
+        for(int i = 0; i < m; i++) {
+
+            // Check if current plot is empty
+            if(flowerbed[i] == 0) {
+
+                // Check left side:
+                // If we are at index 0 OR left plot is 0 (empty)
+                bool left = (i == 0) || (flowerbed[i - 1] == 0);
+
+                // Check right side:
+                // If we are at last index OR right plot is 0 (empty)
+                bool right = (i == m - 1) || (flowerbed[i + 1] == 0);
+
+                // If both left and right conditions allow, we can plant flower here
+                if(left && right) {
+                    flowerbed[i] = 1; // Plant flower
+                    cnt++;            // Increase planted flower count
+                }
             }
         }
-        return cnt>=n;
+
+        // If planted flowers are >= required flowers, return true
+        return cnt >= n;
     }
 };
