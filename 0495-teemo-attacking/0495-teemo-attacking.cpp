6ab13108aab14a7c10d6e07e3d26f816834f5df2
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-05 19:09:26 +0530

    Time: 0 ms (100%), Space: 29.5 MB (93.04%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.cpp
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
-        int n=timeSeries.size();
-        int total=0;
-        for(int i=0;i<n-1;i++){
-            total+=min(timeSeries[i+1]-timeSeries[i],duration);
+        int n = timeSeries.size();
+        int total = 0;
+
+        // For every attack except the last one
+        // We add the poisoning time, but it can overlap with the next attack
+        // So we add the minimum of:
+        //   1) Full duration
+        //   2) Time gap between two attacks (timeSeries[i+1] - timeSeries[i])
+        for(int i = 0; i < n - 1; i++){
+            total += min(timeSeries[i + 1] - timeSeries[i], duration);
         }
-        return total+duration;
+
+        // Last attack always adds full duration
+        return total + duration;
     }
 };
