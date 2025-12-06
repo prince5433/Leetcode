3b2229ba6998647a02f4a36c71244cce0ec4aad3
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-06 21:55:13 +0530

    Time: 0 ms (100%), Space: 9.6 MB (8.33%) - LeetHub

--- 3765-complete-prime-number/3765-complete-prime-number.cpp
+++ 3765-complete-prime-number/3765-complete-prime-number.cpp
@@ -3,19 +3,17 @@ public:
     #define ll long long
 
     // Function to check if a number is prime
-    bool isPrime(int n) {
-        if (n <= 1) return false;         // 0,1 are not prime
-        if (n <= 3) return true;          // 2 and 3 are prime
-        if (n % 2 == 0 || n % 3 == 0)     // eliminate multiples of 2 and 3
-            return false;
-
-        // Check only numbers of form 6k ± 2
-        for (ll i = 5; i * i <= n; i += 6)
-            if (n % i == 0 || n % (i + 2) == 0)
-                return false;
-
-        return true;
-    }
+    int fac=0;
+	bool isPrime(int n){
+	    if(n==1) return false;
+	    for(int i=2;i<=sqrt(n);i++){
+	        if(n%i==0){
+	            // fac=i;
+	            return false;
+	        }
+	    }
+	    return true;
+	}
 
     // Function to check if num is a "complete prime"
     bool completePrime(int num) {
