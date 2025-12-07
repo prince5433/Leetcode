8cadc5041a7dcb8a5c5c7390ad46522f6ab56489
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-07 12:11:36 +0530

    Time: 7 ms (6.96%), Space: 11.7 MB (60.31%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     string removeTrailingZeros(string num) {
-        int n=num.size();
-        int idx=-1;
-        for(int i=n-1;i>=0;i--){
-            if(num[i]!='0'){
-                idx=i;
+
+        // Length of the string (number)
+        int n = num.size();
+        
+        // Variable to store the index of the last NON-zero digit
+        int idx = -1;
+
+        // Traverse from the end of the string backwards
+        for (int i = n - 1; i >= 0; i--) {
+            
+            // As soon as we find a digit that is not '0'
+            // that means trailing zeros end here
+            if (num[i] != '0') {
+                idx = i;
                 break;
             }
         }
-        string t=num.substr(0,idx+1);
+
+        // Take substring from 0 to idx (inclusive)
+        // All trailing zeros are removed
+        string t = num.substr(0, idx + 1);
+
         return t;
     }
 };
