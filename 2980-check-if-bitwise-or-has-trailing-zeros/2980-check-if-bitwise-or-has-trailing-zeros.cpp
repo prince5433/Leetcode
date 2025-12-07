362a6f21e5aedfc5de196aa6afab47c48cde9a2d
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-07 12:15:11 +0530

    Time: 0 ms (100%), Space: 28.9 MB (75.28%) - LeetHub

--- 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.cpp
+++ 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     bool hasTrailingZeros(vector<int>& nums) {
-        int n=nums.size();
-        int ec=0;
-        for(int num:nums){
-            if(num%2==0) ec++;
+        
+        // Size of the input array
+        int n = nums.size();
+        
+        // Counter to count how many even numbers are present in the array
+        int ec = 0;
+
+        // Loop through each number in the array
+        for (int num : nums) {
+
+            // If the number is even (i.e., divisible by 2)
+            // then increment the even counter
+            if (num % 2 == 0) ec++;
         }
-        return ec>=2;
+
+        // If we have at least 2 even numbers,
+        // then it is possible to create a number with trailing zeros after multiplication
+        return ec >= 2;
     }
 };
