52ebd6aed9de69fa17f060e2b0be4d1015ac57cb
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-08 10:39:54 +0530

    Time: 3 ms (90.49%), Space: 7.9 MB (21.68%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int countTriples(int n) {
-        int cnt=0;
-        for(int i=1;i<=n;i++){
-            for(int j=i+1;j<=n;j++){
-               int c=i*i+j*j;
-               int d=sqrt(c);
-               if(d*d==c&&d<=n) cnt++;
+        int cnt = 0;
+
+        // Iterate for all possible values of 'i' and 'j'
+        // such that i < j (unique combinations)
+        for(int i = 1; i <= n; i++){
+            for(int j = i + 1; j <= n; j++){
+
+                // Compute c = i^2 + j^2
+                int c = i * i + j * j;
+
+                // Find sqrt(c) to check if c is a perfect square
+                int d = sqrt(c);
+
+                // If d^2 == c  AND  d <= n,
+                // then (i, j, d) is a valid Pythagorean triple
+                if(d * d == c && d <= n) 
+                    cnt++;
             }
         }
-        return cnt*2;
+
+        // Multiply by 2 because (i, j, d) and (j, i, d) are both valid
+        return cnt * 2;
     }
 };
