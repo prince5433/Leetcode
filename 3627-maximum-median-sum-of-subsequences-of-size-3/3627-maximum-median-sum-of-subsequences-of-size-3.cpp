43288e789b4c665604507853b86f98b131ebc80c
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-13 10:26:58 +0530

    Time: 34 ms (98.2%), Space: 174.3 MB (66.58%) - LeetHub

--- 3627-maximum-median-sum-of-subsequences-of-size-3/3627-maximum-median-sum-of-subsequences-of-size-3.cpp
+++ 3627-maximum-median-sum-of-subsequences-of-size-3/3627-maximum-median-sum-of-subsequences-of-size-3.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     long long maximumMedianSum(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        long long sum=0;
-        for(int i=n-2;i>=(n/3);i-=2){
-            sum+=nums[i];
+        int n = nums.size();
+        
+        // Sort the array so we can pick medians greedily
+        sort(nums.begin(), nums.end());
+        
+        long long sum = 0;
+        
+        // We need exactly n/3 medians.
+        // Start from index (n-2) because the largest element
+        // cannot be a median in any group of 3.
+        // Each time, move left by 2 to skip the largest of the group.
+        for (int i = n - 2; i >= (n / 3); i -= 2) {
+            sum += nums[i];   // add the chosen median
         }
+        
+        // Return the maximum possible sum of medians
         return sum;
     }
 };
