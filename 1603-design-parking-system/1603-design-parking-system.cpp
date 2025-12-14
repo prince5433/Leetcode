f07912eb51c2f82bcaab6b46ad7129193675309c
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-15 00:45:56 +0530

    Time: 16 ms (5.71%), Space: 38.9 MB (10.62%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -1,37 +1,45 @@
 class ParkingSystem {
 public:
-int b,m,s;
+    // b = number of big slots
+    // m = number of medium slots
+    // s = number of small slots
+    int b, m, s;
+
+    // Constructor: initialize parking slots
     ParkingSystem(int big, int medium, int small) {
-        b=big;
-        m=medium;
-        s=small;
+        b = big;        // store big car slots
+        m = medium;     // store medium car slots
+        s = small;      // store small car slots
     }
     
+    // Function to add a car of given type
     bool addCar(int carType) {
-        if(carType==1){
-            if(b>0){
-                b--;
-                return true;
+
+        // If car is BIG type
+        if (carType == 1) {
+            if (b > 0) {     // check if big slot is available
+                b--;         // occupy one big slot
+                return true; // car parked successfully
             }
-            return false;
-        } else if(carType==2){
-            if(m>0){
-                m--;
-                return true;
+            return false;    // no big slot available
+        } 
+        
+        // If car is MEDIUM type
+        else if (carType == 2) {
+            if (m > 0) {     // check if medium slot is available
+                m--;         // occupy one medium slot
+                return true; // car parked successfully
             }
-            return false;
-        } else {
-            if(s>0){
-                s--;
-                return true;
+            return false;    // no medium slot available
+        } 
+        
+        // If car is SMALL type
+        else {
+            if (s > 0) {     // check if small slot is available
+                s--;         // occupy one small slot
+                return true; // car parked successfully
             }
-            return false;
+            return false;    // no small slot available
         }
     }
 };
-
-/**
- * Your ParkingSystem object will be instantiated and called as such:
- * ParkingSystem* obj = new ParkingSystem(big, medium, small);
- * bool param_1 = obj->addCar(carType);
- */
