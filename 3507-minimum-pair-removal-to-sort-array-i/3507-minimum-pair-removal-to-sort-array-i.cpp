5b5c5cb369a069312df656d7953f209efdf88457
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-14 10:37:08 +0530

    Time: 0 ms (100%), Space: 26.5 MB (67.77%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
-bool isSorted(vector<int>& nums){
-    for(int i=1;i<nums.size();i++){
-        if(nums[i]<nums[i-1]) return false;
+    // Check if array is non-decreasing (sorted)
+    bool isSorted(vector<int>& nums){
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] < nums[i-1]) 
+                return false;
+        }
+        return true;
     }
-    return true;
-}
+
     int minimumPairRemoval(vector<int>& nums) {
-        int ops=0;
+        int ops = 0;
+
+        // Repeat until array becomes sorted
         while(!isSorted(nums)){
-            int idx=0;
-            int mn=nums[0]+nums[1];
-            for(int i=1;i<nums.size()-1;i++){
-                if(nums[i]+nums[i+1]<mn){
-                    mn=nums[i]+nums[i+1];
-                    idx=i;
+
+            int idx = 0;
+            // Minimum sum of adjacent pair
+            int mn = nums[0] + nums[1];
+
+            // Find adjacent pair with minimum sum
+            for(int i = 1; i < nums.size() - 1; i++){
+                if(nums[i] + nums[i+1] < mn){
+                    mn = nums[i] + nums[i+1];
+                    idx = i;
                 }
             }
-            nums.erase(nums.begin()+idx);
-            nums[idx]=mn;
-            ops++;
+
+            // Remove the selected pair
+            // erase removes nums[idx]
+            nums.erase(nums.begin() + idx);
+
+            // Replace next element with their sum
+            nums[idx] = mn;
+
+            ops++; // one operation done
         }
+
         return ops;
     }
 };
