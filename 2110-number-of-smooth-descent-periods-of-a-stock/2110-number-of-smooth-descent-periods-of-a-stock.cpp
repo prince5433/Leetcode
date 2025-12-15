cd22965c83035298833d68d4b48e4d0e27f98141
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-15 10:35:50 +0530

    Time: 0 ms (100%), Space: 106.9 MB (97.03%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     long long getDescentPeriods(vector<int>& prices) {
-        long long cnt=0;
-        int n=prices.size();
-        long long len=1;
-        for(int i=1;i<n;i++){
-            if(prices[i]==prices[i-1]-1){
-                len++;
-            } else{
-                cnt+=(len*(len+1))/2;
-                len=1;
+        long long cnt = 0;   // total number of descent periods
+        int n = prices.size();
+        long long len = 1;   // length of current descending sequence
+
+        for (int i = 1; i < n; i++) {
+
+            // If current price is exactly 1 less than previous
+            if (prices[i] == prices[i - 1] - 1) {
+                len++;   // extend the descending sequence
+            } 
+            else {
+                // add subarrays formed by previous descending sequence
+                cnt += (len * (len + 1)) / 2;
+                len = 1; // reset for new sequence
             }
         }
-            cnt+=(len*(len+1))/2;
-            return cnt;
+
+        // add subarrays for the last descending sequence
+        cnt += (len * (len + 1)) / 2;
+
+        return cnt;
     }
 };
