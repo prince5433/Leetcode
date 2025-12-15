3302638e8c398e35772e6b05acbfd7e205bcd29e
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-15 10:24:45 +0530

    Time: 0 ms (100%), Space: 21.1 MB (63.07%) - LeetHub

--- 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.cpp
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int countTestedDevices(vector<int>& batteryPercentages) {
-        int n=batteryPercentages.size();
-        int cnt=0;
-        int i=0;
-        while(i<n){
-            if(batteryPercentages[i]>0){
-                cnt++;
-                for(int j=i+1;j<n;j++){
-                    batteryPercentages[j]=max(0,batteryPercentages[j]-1);
+        int n = batteryPercentages.size();
+        int cnt = 0;
+        int i = 0;
+
+        while (i < n) {
+            // If current device has battery > 0
+            if (batteryPercentages[i] > 0) {
+                cnt++;   // we test this device
+
+                // After testing, all next devices lose 1% battery
+                for (int j = i + 1; j < n; j++) {
+                    batteryPercentages[j] = max(0, batteryPercentages[j] - 1);
                 }
-            } 
+            }
             i++;
         }
         return cnt;
