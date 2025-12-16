a87286c06312166329d2a663e3c68263f048051c
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-16 11:42:28 +0530

    Time: 60 ms (49.53%), Space: 49.2 MB (39.12%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     int numOfPairs(vector<string>& nums, string target) {
-        int n=nums.size();
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(nums[i]+nums[j]==target&& i!=j) cnt++;
+
+        int n = nums.size();
+        int cnt = 0;
+
+        // Pick first string
+        for(int i = 0; i < n; i++){
+
+            // Pick second string
+            for(int j = 0; j < n; j++){
+
+                // i != j ensures we are using different indices
+                // Check if concatenation equals target
+                if(i != j && nums[i] + nums[j] == target){
+                    cnt++;
+                }
             }
         }
+
+        // Total valid pairs
         return cnt;
     }
 };
