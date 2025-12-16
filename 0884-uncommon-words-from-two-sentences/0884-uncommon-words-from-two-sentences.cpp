518c78c61ac68bf49bec404a32516b8e48e33b42
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-16 11:15:06 +0530

    Time: 0 ms (100%), Space: 9.4 MB (53.13%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
-          unordered_map<string,int>mp1;
-          stringstream ss(s1);
-          string temp;
-          while(ss>>temp){
-            mp1[temp]++;
-          }
-            stringstream sp(s2);
-          string temp1;
-          while(sp>>temp1){
-            mp1[temp1]++;
-          }
-          vector<string> ans;
 
-          for(auto& it:mp1){
-            if(it.second==1){
+        // Map to store frequency of each word from both sentences
+        unordered_map<string,int> mp1;
+
+        // Stringstream to break first sentence into words
+        stringstream ss(s1);
+        string temp;
+
+        // Count words from s1
+        while (ss >> temp) {
+            mp1[temp]++;
+        }
+
+        // Stringstream to break second sentence into words
+        stringstream sp(s2);
+
+        // Count words from s2
+        while (sp >> temp) {
+            mp1[temp]++;
+        }
+
+        // Vector to store uncommon words
+        vector<string> ans;
+
+        // A word is uncommon if it appears exactly once in total
+        for (auto& it : mp1) {
+            if (it.second == 1) {
                 ans.push_back(it.first);
             }
-          }
-          return ans;
+        }
+
+        // Return list of uncommon words
+        return ans;
     }
 };
