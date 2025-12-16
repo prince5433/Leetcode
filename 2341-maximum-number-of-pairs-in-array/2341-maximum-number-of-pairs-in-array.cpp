d39c84aab015896d4f235c3e164f39bcd94a9382
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-16 11:03:42 +0530

    Time: 0 ms (100%), Space: 12.1 MB (24.66%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     vector<int> numberOfPairs(vector<int>& nums) {
+
+        // Map to store frequency of each number
         map<int,int> mp;
-        for(int num:nums)mp[num]++;
+        for(int num : nums)
+            mp[num]++;
+
+        // ans[0] -> number of pairs
+        // ans[1] -> number of leftover elements
         vector<int> ans(2);
-        int cnt1,cnt2=0;
-        for(auto &it:mp){
-           cnt1+=it.second/2;
-           cnt2+=it.second%2;
+
+        int cnt1 = 0, cnt2 = 0;
+
+        // Traverse frequency map
+        for(auto &it : mp){
+            cnt1 += it.second / 2;   // pairs
+            cnt2 += it.second % 2;   // leftovers
         }
-        ans[0]=cnt1;
-        ans[1]=cnt2;
+
+        ans[0] = cnt1;
+        ans[1] = cnt2;
         return ans;
     }
 };
