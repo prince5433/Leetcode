17e699199b536904fcaf8aa78ef8309b4b8b8461
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-17 10:48:47 +0530

    Time: 0 ms (100%), Space: 7.9 MB (74.69%) - LeetHub

--- 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.cpp
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     bool digitCount(string num) {
-        int n=num.size();
-        vector<int> freq(10,0);
-        for(char c:num){
-            freq[c-'0']++;
+        int n = num.size();   // length of the string
+
+        // freq[d] will store how many times digit 'd' appears in the string
+        vector<int> freq(10, 0);
+
+        // Count frequency of each digit in num
+        for(char c : num){
+            freq[c - '0']++;
         }
-        for(int i=0;i<n;i++){
-            if(freq[i]!=num[i]-'0') return false; 
+
+        // Check the condition:
+        // At index i, digit i should appear exactly (num[i] - '0') times
+        for(int i = 0; i < n; i++){
+            if(freq[i] != num[i] - '0')
+                return false;   // condition fails
         }
-        return true;
+
+        return true;   // all conditions satisfied
     }
 };
