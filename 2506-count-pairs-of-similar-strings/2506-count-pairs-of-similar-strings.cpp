d8f34c5d3df47b26626b6ebe8f4a2b93b9af0cd2
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-17 11:15:16 +0530

    Time: 994 ms (9.79%), Space: 290.1 MB (10.39%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cpp
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int similarPairs(vector<string>& words) {
-        int cnt=0;
-        int n=words.size();
-        for(int i=0;i<n-1;i++){
-            set<char> s1(words[i].begin(),words[i].end());
-         
-            for(int j=i+1;j<n;j++){
-              set <char>s2(words[j].begin(),words[j].end());
-              if(s1==s2) cnt++;
+        int cnt = 0;                 // to count number of similar pairs
+        int n = words.size();        // total number of words
+
+        // Pick first word of the pair
+        for(int i = 0; i < n - 1; i++){
+            // Create a set of unique characters for words[i]
+            set<char> s1(words[i].begin(), words[i].end());
+
+            // Compare with all words after index i
+            for(int j = i + 1; j < n; j++){
+                // Create a set of unique characters for words[j]
+                set<char> s2(words[j].begin(), words[j].end());
+
+                // If both sets are equal, the pair is similar
+                if(s1 == s2)
+                    cnt++;
             }
         }
-        return cnt;
+
+        return cnt;   // return total similar pairs
     }
 };
