70b4d1244afb7a9e4308162fc0367fbc49cab462
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-17 10:40:43 +0530

    Time: 119 ms (10.95%), Space: 32 MB (21.9%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        map<char,int> mp1;
-        for(char c:s) mp1[c]++;
-        for(char c:t) mp1[c]--;
-        int ans=0;
-        for(auto it:mp1){
-            ans+=abs(it.second);
+        map<char,int> mp1;   // map to store frequency difference of characters
+
+        // Count frequency of characters in string s
+        for(char c : s)
+            mp1[c]++;
+
+        // Subtract frequency of characters in string t
+        for(char c : t)
+            mp1[c]--;
+
+        int ans = 0;   // to store total minimum steps (deletions)
+
+        // Sum absolute differences of all character frequencies
+        for(auto it : mp1){
+            ans += abs(it.second);
         }
-        return ans;
+
+        return ans;   // return minimum number of steps
     }
 };
