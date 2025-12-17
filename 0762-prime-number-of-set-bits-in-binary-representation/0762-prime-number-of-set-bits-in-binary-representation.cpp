4a5e0b46ee9437cdc84d9441000d0e85164bfaf6
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-18 00:46:47 +0530

    Time: 2 ms (71.33%), Space: 7.9 MB (62.01%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
-bool isPrime(int n){
-    if(n<=1) return false;
-    for(int i=2;i*i<=n;i++){
-        if(n%i==0)return false;
-    }
-    return true;
-}
-    int countPrimeSetBits(int left, int right) {
-        int cnt=0;
-        for(int i=left;i<=right;i++){
-            if(isPrime(__builtin_popcount(i))) cnt++;
+
+    // Function to check whether a number is prime
+    bool isPrime(int n){
+        // 0 and 1 are not prime
+        if(n <= 1) return false;
+
+        // Check divisibility from 2 up to sqrt(n)
+        for(int i = 2; i * i <= n; i++){
+            if(n % i == 0)
+                return false;   // Not prime
         }
+        return true;            // Prime number
+    }
+
+    int countPrimeSetBits(int left, int right) {
+
+        int cnt = 0;  // To count numbers whose set bits are prime
+
+        // Iterate from left to right (inclusive)
+        for(int i = left; i <= right; i++){
+
+            // __builtin_popcount(i) returns number of set bits (1s) in binary of i
+            int setBits = __builtin_popcount(i);
+
+            // Check if number of set bits is prime
+            if(isPrime(setBits))
+                cnt++;
+        }
+
+        // Return final count
         return cnt;
     }
 };
