d892523cd44fba965d64aa408eaf2ebd5c4affe5
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-18 18:25:36 +0530

    Time: 0 ms (100%), Space: 8.8 MB (10.73%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-int product(int n){
-    int prod = 1;        // start with 1, not 0
-    while(n > 0){
-        int digit = n % 10;   // extract last digit
-        prod *= digit;        // multiply
-        n /= 10;              // remove last digit
-    }
-    return prod;              // return the product
-}
-
-    int smallestNumber(int n, int t) {
-        int i=n;
-        while(true){
-            int prod=product(i);
-            if(prod%t==0) {
-                return i;
-            break;
-            }
-            i++;
-         
+    // Function to calculate product of digits of a number
+    int product(int n){
+        int prod = 1;              // Initialize product as 1
+        while(n > 0){
+            int digit = n % 10;    // Get last digit
+            prod *= digit;         // Multiply with product
+            n /= 10;               // Remove last digit
         }
-        return 0;
+        return prod;               // Return digit product
+    }
+
+    // Function to find the smallest number >= n
+    // whose digit product is divisible by t
+    int smallestNumber(int n, int t) {
+        int i = n;
+        while(true){
+            int prod = product(i);   // Get product of digits
+            if(prod % t == 0) {       // Check divisibility
+                return i;             // Return answer
+            }
+            i++;                      // Try next number
+        }
     }
 };
