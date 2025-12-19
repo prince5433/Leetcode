913afd978b270e366b45236c06b39d10edccb311
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-19 10:53:11 +0530

    Time: 83 ms (33.53%), Space: 87.3 MB (93.17%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> ans(n);
-        int ts=0;
-        for(int x:nums) ts+=x;
-        int leftsum=0;
-        for(int i=0;i<n;i++){
-            int left=((i*nums[i])-leftsum);
-            int right=((ts-leftsum-nums[i])-(n-i-1)*nums[i]);
-            ans[i]=left+right;
-            leftsum+=nums[i];
+
+        int n = nums.size();          // size of array
+        vector<int> ans(n);           // answer array
+
+        int ts = 0;                   // ts = total sum of array
+        for(int x : nums) 
+            ts += x;
+
+        int leftsum = 0;              // sum of elements on left side
+
+        for(int i = 0; i < n; i++) {
+
+            // LEFT PART:
+            // nums[i] se left ke sab elements ka difference
+            // formula: i * nums[i] - leftsum
+            int left = (i * nums[i]) - leftsum;
+
+            // RIGHT PART:
+            // nums[i] se right ke sab elements ka difference
+            // formula: (sum of right elements) - (count * nums[i])
+            int right = (ts - leftsum - nums[i]) 
+                        - (n - i - 1) * nums[i];
+
+            // total absolute difference for index i
+            ans[i] = left + right;
+
+            // update leftsum for next iteration
+            leftsum += nums[i];
         }
+
         return ans;
     }
 };
