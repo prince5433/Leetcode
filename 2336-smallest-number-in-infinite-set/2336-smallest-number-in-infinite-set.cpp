3e96b94493bb07d346d62befc6e85c189f7f111d
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-20 10:44:30 +0530

    Time: 38 ms (15.81%), Space: 54.4 MB (7.03%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -1,28 +1,31 @@
 class SmallestInfiniteSet {
 public:
-set<int> st;
+    set<int> st;   // ordered set → hamesha sorted rahega
+
     SmallestInfiniteSet() {
-        for(int i=1;i<=1000;i++){
+        // initially set me 1 se 1000 tak saare numbers daal diye
+        // constraints ke hisaab se itna kaafi hai
+        for (int i = 1; i <= 1000; i++) {
             st.insert(i);
         }
     }
     
     int popSmallest() {
-        int x=*st.begin();
+        // set ka begin() hamesha smallest element deta hai
+        int x = *st.begin();
+
+        // us smallest element ko set se hata dete hain
         st.erase(st.begin());
+
+        // wahi smallest number return
         return x;
     }
     
     void addBack(int num) {
-        if(st.find(num)==st.end()){
-            st.insert(num);
+        // agar num already set me nahi hai
+        // (matlab pehle pop ho chuka tha)
+        if (st.find(num) == st.end()) {
+            st.insert(num);   // wapas add kar do
         }
     }
 };
-
-/**
- * Your SmallestInfiniteSet object will be instantiated and called as such:
- * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
- * int param_1 = obj->popSmallest();
- * obj->addBack(num);
- */
