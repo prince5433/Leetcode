89a19c30683558a3af1d5600f28bbeb636013b8c
Prince Seth <156539518+prince5433@users.noreply.github.com>
2025-12-23 18:21:05 +0530

    Time: 0 ms (100%), Space: 11.3 MB (31.24%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     int specialArray(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            int x=n-i;
-            if(nums[i]>=x && (i==0||nums[i-1]<x)) return x;
+        int n = nums.size();              // array ka size n
+
+        sort(nums.begin(), nums.end());   // array ko ascending order me sort kar diya
+
+        for(int i = 0; i < n; i++) {
+            int x = n - i;                // x = number of elements from index i to end
+
+            // condition:
+            // nums[i] >= x  -> i se end tak sab elements >= x ho sakte hain
+            // nums[i-1] < x -> i se pehle wale elements x se chhote hone chahiye
+            if(nums[i] >= x && (i == 0 || nums[i-1] < x))
+                return x;                 // special value mil gayi
         }
-        return -1;
+
+        return -1;                        // koi bhi x valid nahi mila
     }
 };
