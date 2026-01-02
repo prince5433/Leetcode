c11918a5be8a5770345cb7ae35acf7f38f507162
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-01-02 22:02:54 +0530

    Time: 566 ms (7.35%), Space: 187.1 MB (8.34%) - LeetHub

--- 3790-smallest-all-ones-multiple/3790-smallest-all-ones-multiple.cpp
+++ 3790-smallest-all-ones-multiple/3790-smallest-all-ones-multiple.cpp
@@ -2,27 +2,31 @@ class Solution {
 public:
     int minAllOneMultiple(int k) {
 
-        // agar k 2 ya 5 se divisible hai → impossible
-        // if (k % 2 == 0 || k % 5 == 0) return -1;
-
-        int rem = 1 % k;
-        int cone = 1;
+        int rem = 1 % k;   // "1" ka remainder jab k se divide karein
+        int cone = 1;      // abhi number me 1 digit hai (sirf "1")
 
         unordered_set<int> st;
-        st.insert(rem);
+        st.insert(rem);    // pehla remainder store (cycle detect ke liye)
 
         while (true) {
 
-            if (rem == 0) return cone;   // divisible mil gaya
+            // agar remainder 0 aa gaya
+            // matlab current number k se divisible hai
+            if (rem == 0) return cone;
 
+            // next number banate hain:
+            // previous number * 10 + 1
+            // remainder update karte hain bina number banaye
             rem = (rem * 10 + 1) % k;
-            cone++;
+            cone++;        // ek aur '1' add hua
 
-            if (st.count(rem)) {        // cycle detect
+            // agar remainder repeat ho gaya
+            // matlab loop ban gaya, 0 kabhi nahi aayega
+            if (st.count(rem)) {
                 return -1;
             }
 
-            st.insert(rem);             // naye remainder ko store karo
+            st.insert(rem); // naye remainder ko set me daal do
         }
     }
 };
