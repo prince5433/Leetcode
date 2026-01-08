7d2fee0580fdcbfe5382230008d02a3110c22dea
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-01-08 21:55:46 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.59%) - LeetHub

--- 3798-largest-even-number/3798-largest-even-number.cpp
+++ 3798-largest-even-number/3798-largest-even-number.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     string largestEven(string s) {
-        int n = s.size();
+        int n = s.size();   // length of the string
+
+        // Traverse from right to left
+        // because we want the rightmost even digit
         for(int i = n - 1; i >= 0; i--) {
+
+            // Convert character digit to integer
+            // and check if it is even
             if ((s[i] - '0') % 2 == 0) {
+
+                // Return the prefix ending at this even digit
+                // This forms the largest possible even number
                 return s.substr(0, i + 1);
             }
         }
+
+        // If no even digit is found, return empty string
         return "";
     }
 };
