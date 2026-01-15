72dc4a0f05a51fa520ac0720c8be3be75f44260c
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-01-15 12:27:59 +0530

    Time: 4 ms (87.94%), Space: 181.8 MB (74.37%) - LeetHub

--- 3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i/3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp
+++ 3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i/3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     int minFlips(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int rf=0;
-        for(int row=0;row<n;row++){
-          int l=0;
-          int r=m-1;
-          while(l<r){
- if(grid[row][l]!=grid[row][r]) {
-                rf++;
-                
-            }
-            l++;
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int rf = 0;  // flips needed to make all rows palindromic
+        for(int row = 0; row < n; row++) {
+            int l = 0, r = m - 1;
+            while(l < r) {
+                if(grid[row][l] != grid[row][r]) {
+                    rf++;  // one flip fixes this mismatched pair
+                }
+                l++;
                 r--;
-        }
-          }
-           
-         int cf=0;
-        for(int col=0;col<m;col++){
-          int l=0;
-          int r=n-1;
-          while(l<r){
-             if(grid[l][col]!=grid[r][col]) {
-                cf++;
             }
-              l++;
-                r--;
         }
-          }
-           
-        return min(rf,cf);
+
+        int cf = 0;  // flips needed to make all columns palindromic
+        for(int col = 0; col < m; col++) {
+            int l = 0, r = n - 1;
+            while(l < r) {
+                if(grid[l][col] != grid[r][col]) {
+                    cf++;  // one flip fixes this mismatched pair
+                }
+                l++;
+                r--;
+            }
+        }
+
+        // choose the minimum flips between rows or columns
+        return min(rf, cf);
     }
 };
