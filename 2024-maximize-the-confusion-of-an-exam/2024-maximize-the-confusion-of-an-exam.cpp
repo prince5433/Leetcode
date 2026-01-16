adc3d3d3e299de5c570eff333eb22b0a0bf8dcf0
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-01-16 13:08:27 +0530

    Time: 12 ms (36.21%), Space: 12.6 MB (53.98%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,41 +1,49 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-        int n=answerKey.size();
-        int i=0,j=0;
-        int cntf=0;
-        int result=0;
-        //f->t
-        while(j<n){
-            if(answerKey[j]=='F'){
-                cntf++;
+        int n = answerKey.size();
+
+        int i = 0, j = 0;
+        int cntf = 0;
+        int result = 0;
+
+        // Case 1: F ko flip karke window ko all 'T' banane ki try
+        // condition: window me F ki count <= k
+        while (j < n) {
+            if (answerKey[j] == 'F') cntf++;
+
+            // agar F zyada ho gaye (k se), to window shrink karo
+            while (cntf > k) {
+                if (answerKey[i] == 'F') cntf--;
+                i++;
             }
-            while(cntf>k){
-                if(answerKey[i]=='F'){
-                    cntf--;
-                }
-                 i++;
-            }
-       
-            result=max(result,j-i+1);
-                 j++;
-        }
-        i=0,j=0;
-       int cntt=0;
-         while(j<n){
-            if(answerKey[j]=='T'){
-                cntt++;
-            }
-            while(cntt>k){
-                if(answerKey[i]=='T'){
-                    cntt--;
-                }
-                 i++;
-            }
-            
-            result=max(result,j-i+1);
+
+            // valid window length update
+            result = max(result, j - i + 1);
             j++;
         }
+
+        // reset pointers for second case
+        i = 0; 
+        j = 0;
+        int cntt = 0;
+
+        // Case 2: T ko flip karke window ko all 'F' banane ki try
+        // condition: window me T ki count <= k
+        while (j < n) {
+            if (answerKey[j] == 'T') cntt++;
+
+            // agar T zyada ho gaye (k se), to window shrink karo
+            while (cntt > k) {
+                if (answerKey[i] == 'T') cntt--;
+                i++;
+            }
+
+            // valid window length update
+            result = max(result, j - i + 1);
+            j++;
+        }
+
         return result;
     }
 };
