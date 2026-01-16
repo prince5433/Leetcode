879163bd0c6ddba72b9e8dab21aece092d4f0d6b
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-01-16 18:42:45 +0530

    Time: 39 ms (5.73%), Space: 12.6 MB (26.8%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -2,48 +2,30 @@ class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
         int n = answerKey.size();
-
         int i = 0, j = 0;
-        int cntf = 0;
-        int result = 0;
+        int res = 0;
 
-        // Case 1: F ko flip karke window ko all 'T' banane ki try
-        // condition: window me F ki count <= k
+        unordered_map<char,int> mp; 
+        // mp['T'] = current window me T count
+        // mp['F'] = current window me F count
+
         while (j < n) {
-            if (answerKey[j] == 'F') cntf++;
+            // right pointer add karo
+            mp[answerKey[j]]++;
 
-            // agar F zyada ho gaye (k se), to window shrink karo
-            while (cntf > k) {
-                if (answerKey[i] == 'F') cntf--;
+            // min(T,F) = window me jo minority character hai
+            // us minority ko flip karke window ko all same banaya ja sakta hai
+            // agar minority > k ho gaya => k flips se possible nahi => shrink window
+            while (min(mp['T'], mp['F']) > k) {
+                mp[answerKey[i]]--;
                 i++;
             }
 
-            // valid window length update
-            result = max(result, j - i + 1);
+            // valid window ka max length update
+            res = max(res, j - i + 1);
             j++;
         }
 
-        // reset pointers for second case
-        i = 0; 
-        j = 0;
-        int cntt = 0;
-
-        // Case 2: T ko flip karke window ko all 'F' banane ki try
-        // condition: window me T ki count <= k
-        while (j < n) {
-            if (answerKey[j] == 'T') cntt++;
-
-            // agar T zyada ho gaye (k se), to window shrink karo
-            while (cntt > k) {
-                if (answerKey[i] == 'T') cntt--;
-                i++;
-            }
-
-            // valid window length update
-            result = max(result, j - i + 1);
-            j++;
-        }
-
-        return result;
+        return res;
     }
 };
