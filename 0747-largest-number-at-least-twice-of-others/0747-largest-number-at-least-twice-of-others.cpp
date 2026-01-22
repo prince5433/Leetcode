6707129ec0f4bd68eaa34521096c7213a2d6eb21
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-01-22 22:57:10 +0530

    Time: 0 ms (100%), Space: 13.8 MB (48.62%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int dominantIndex(vector<int>& nums) {
-        int n=nums.size();
-    int maxidx=-1;
-    int maxi=-1;
-    for(int i=0;i<n;i++){
-        if(nums[i]>maxi){
-            maxi=nums[i];
-            maxidx=i;
+        int n = nums.size();
+
+        int maxidx = -1;   // max element ka index (original array me)
+        int maxi = -1;     // max element ki value
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] > maxi){
+                maxi = nums[i];   // new maximum update
+                maxidx = i;       // uska index store
+            }
         }
-    }
-    sort(nums.begin(),nums.end());
-    if(nums[n-1]>=2*nums[n-2]) return maxidx;
-    else return -1;
+
+        sort(nums.begin(), nums.end());  // array sort kar diya (order change ho gaya)
+
+        // sorted array me last element max hota hai
+        // second last element second max hota hai
+        // condition check: max >= 2 * secondMax
+        if(nums[n-1] >= 2 * nums[n-2]) 
+            return maxidx;   // original array ka index return kar rahe ho
+        else 
+            return -1;       // dominant nahi hai
     }
 };
