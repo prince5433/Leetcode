0e9deeb86fbb80c71c63cbde1c449c58030510dd
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-01-23 21:29:12 +0530

    Time: 0 ms (100%), Space: 8.2 MB (3.28%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     vector<int> constructRectangle(int area) {
-         vector<int> ans;
-         int finde=sqrt(area);
-         for(int i=finde;i>=1;i--){
-            int d1=i;
-            int d2=area/i;
-            if(d1*d2==area){
-              ans.push_back(d1);
-            ans.push_back(d2);
-            break;
+        vector<int> ans;                    // answer store karega [L, W]
+
+        int finde = sqrt(area);             // sqrt(area) se start karenge (closest factors milte hain)
+
+        for(int i = finde; i >= 1; i--){    // i ko sqrt se 1 tak check kar rahe
+            int d1 = i;                     // possible width
+            int d2 = area / i;              // corresponding length
+
+            // agar d1 factor hai to d1*d2 exactly area hoga
+            if(d1 * d2 == area){
+                ans.push_back(d1);          // factor 1 push
+                ans.push_back(d2);          // factor 2 push
+                break;                      // first valid pair best hota hai (min difference)
             }
-         }
-         sort(ans.rbegin(),ans.rend());
-         return ans;
+        }
+
+        sort(ans.rbegin(), ans.rend());     // descending sort so ans[0] = L (bigger), ans[1] = W (smaller)
+        return ans;                         // return [L, W]
     }
 };
