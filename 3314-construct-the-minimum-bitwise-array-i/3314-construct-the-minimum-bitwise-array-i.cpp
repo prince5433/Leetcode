78704e053e8d7635596edca4625ef683da2a9fd3
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-02-02 20:42:50 +0530

    Time: 2 ms (38.61%), Space: 25.1 MB (53.54%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
-        int n=nums.size();
-    vector<int> ans(n,-1);
-    for(int i=0;i<n;i++){
-        for(int j=1;j<=nums[i];j++){
-           if ( (j | (j + 1)) == nums[i] ){
-                 ans[i]=j;
-                 break;
+
+        int n = nums.size();                 // nums array ka size
+        vector<int> ans(n, -1);              // answer array, default -1
+
+        // Har element ke liye
+        for(int i = 0; i < n; i++){
+
+            // j ko 1 se nums[i] tak try kar rahe hain
+            for(int j = 1; j <= nums[i]; j++){
+
+                // Check: j OR (j+1) agar nums[i] ke equal ho
+                // Brackets zaroori hain warna operator precedence galat ho jaayegi
+                if ( (j | (j + 1)) == nums[i] ){
+
+                    ans[i] = j;              // smallest j mil gaya
+                    break;                  // aage check karne ki zarurat nahi
+                }
             }
         }
-    }
-    return ans;
+
+        return ans;                          // final result return
     }
 };
