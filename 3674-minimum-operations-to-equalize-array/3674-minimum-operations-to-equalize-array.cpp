b8ad5d6ce3eeb9fdb0702a7967c80b61d3369861
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-02-06 23:56:04 +0530

    Time: 0 ms (100%), Space: 30.8 MB (67.23%) - LeetHub

--- 3674-minimum-operations-to-equalize-array/3674-minimum-operations-to-equalize-array.cpp
+++ 3674-minimum-operations-to-equalize-array/3674-minimum-operations-to-equalize-array.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int n=nums.size();
-      int i=0;
-      while(i<n-1){
-            if(nums[i]==nums[i+1]){
+        int n = nums.size();          // Array ka size store kar liya
+
+        int i = 0;                    // Pointer start se rakha
+        while (i < n - 1) {           // Last se ek pehle tak check karenge
+            if (nums[i] == nums[i + 1]) {
+                // Agar current element aur next element same hain
+                // to aage badho
                 i++;
-            }
-            else {
+            } else {
+                // Jaise hi koi adjacent element different mila
+                // loop tod do
                 break;
             }
         }
-        if(i==n-1) return 0;
-        else return 1;
+
+        // Agar i last index tak pahunch gaya
+        // matlab saare elements same the
+        if (i == n - 1) 
+            return 0;                 // Koi operation nahi chahiye
+        else 
+            return 1;                 // Kam se kam 1 operation lagega
     }
 };
