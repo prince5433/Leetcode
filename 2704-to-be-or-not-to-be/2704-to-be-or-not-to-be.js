af47cb50904562d9a9cb83799817ba4cda437992
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-02-11 10:13:48 +0530

    Time: 47 ms (36.58%), Space: 54 MB (28.67%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,12 @@
+var expect = function(val) {
+    return {
+        toBe: function(val2) {
+            if (val === val2) return true;
+            throw new Error("Not Equal");
+        },
+        notToBe: function(val2) {
+            if (val !== val2) return true;
+            throw new Error("Equal");
+        }
+    };
+};
