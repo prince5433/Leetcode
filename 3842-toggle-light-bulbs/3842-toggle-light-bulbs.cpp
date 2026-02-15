54ffac0525b76b48976abe514aa875ff6c1c5c04
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-02-15 13:16:27 +0530

    Time: 11 ms (9.74%), Space: 34.8 MB (6.76%) - LeetHub

--- 3842-toggle-light-bulbs/3842-toggle-light-bulbs.cpp
+++ 3842-toggle-light-bulbs/3842-toggle-light-bulbs.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
     vector<int> toggleLightBulbs(vector<int>& bulbs) {
+
+        // Vector to store bulbs which remain ON (odd toggles)
         vector<int> ans;
-        map<int,int>mp;
-        for(int num:bulbs)mp[num]++;
-        for(auto it:mp){
-            if(it.second%2!=0){
+
+        // map to store frequency of each bulb number
+        // key   -> bulb number
+        // value -> number of times toggled
+        map<int,int> mp;
+
+        // Count how many times each bulb is toggled
+        for(int num : bulbs)
+            mp[num]++;
+
+        // Traverse the map
+        for(auto it : mp){
+
+            // If toggled odd number of times,
+            // bulb will remain ON
+            if(it.second % 2 != 0){
+
+                // Store bulb number in answer
                 ans.push_back(it.first);
             }
         }
+
+        // Return list of bulbs that remain ON
         return ans;
     }
 };
