a364e4aa6ad8778f49ba42bcb24758b936e45ce5
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-02-28 23:39:23 +0530

    Time: 6 ms (36.37%), Space: 30.9 MB (60.45%) - LeetHub

--- 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.cpp
+++ 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
     vector<int> minDistinctFreqPair(vector<int>& nums) {
-        vector<int> ans;
-        int n=nums.size();
-        if(n<=2) return {-1,-1};
-        map<int,int> mp;
-        for(int num:nums) mp[num]++;
-        sort(nums.begin(),nums.end());
-        ans.push_back(nums[0]);
-        int comp=nums[0];
-        int val=-1;
-        for(auto it:mp){
-            if(it.first==comp){
-                val=it.second;
+        
+        vector<int> ans;                  // answer store karne ke liye
+        
+        int n = nums.size();
+        
+        // Agar elements 2 ya usse kam hain to valid pair possible nahi
+        if(n <= 2) return {-1, -1};
+        
+        map<int,int> mp;                  // value -> frequency store karega
+        
+        // Frequency count
+        for(int num : nums) 
+            mp[num]++;
+        
+        // Smallest x lene ke liye sort kar rahe hain
+        sort(nums.begin(), nums.end());
+        
+        ans.push_back(nums[0]);           // smallest value ko x maan liya
+        int comp = nums[0];               // x store kiya
+        
+        int val = -1;                     // x ki frequency store karne ke liye
+        
+        // x ki frequency nikaal rahe hain
+        for(auto it : mp){
+            if(it.first == comp){
+                val = it.second;
             }
         }
-        for(auto it:mp){
-            if(it.second!=val){
-                 ans.push_back(it.first);
-                 return ans;
+        
+        // Ab smallest y find kar rahe hain
+        // jiska frequency x se different ho
+        for(auto it : mp){
+            if(it.second != val){
+                ans.push_back(it.first);  // valid y mil gaya
+                return ans;
             }
         }
-        return {-1,-1};
+        
+        // Agar koi valid pair nahi mila
+        return {-1, -1};
     }
 };
