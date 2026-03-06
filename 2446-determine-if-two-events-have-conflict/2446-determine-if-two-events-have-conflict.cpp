59fb3170c0ae29997c24bc4fea4f3f3b2291f3be
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-03-07 00:00:08 +0530

    Time: 0 ms (100%), Space: 14 MB (14.89%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
     bool haveConflict(vector<string>& event1, vector<string>& event2) {
-        vector<int> t1,t2;
-        for(string str:event1){
-            string hour=str.substr(0,2);
-            string min=str.substr(3);
-            int hr=stoi(hour);
-            int mi=stoi(min);
-            int time=60*hr+mi;
+
+        vector<int> t1, t2;
+
+        // convert event1 times into minutes
+        for(string str : event1){
+            string hour = str.substr(0,2);
+            string min = str.substr(3);
+
+            int hr = stoi(hour);
+            int mi = stoi(min);
+
+            int time = 60*hr + mi;
             t1.push_back(time);
         }
-          for(string str:event2){
-            string hour=str.substr(0,2);
-            string min=str.substr(3);
-            int hr=stoi(hour);
-            int mi=stoi(min);
-            int time=60*hr+mi;
+
+        // convert event2 times into minutes
+        for(string str : event2){
+            string hour = str.substr(0,2);
+            string min = str.substr(3);
+
+            int hr = stoi(hour);
+            int mi = stoi(min);
+
+            int time = 60*hr + mi;
             t2.push_back(time);
         }
-   if((t2[0]>=t1[0] && t2[0]<=t1[1])||(t1[0]>=t2[0] && t1[0]<=t2[1])) return true;
-        else return false;
+
+        // check if event2 start lies inside event1
+        // OR event1 start lies inside event2
+        if((t2[0] >= t1[0] && t2[0] <= t1[1]) || 
+           (t1[0] >= t2[0] && t1[0] <= t2[1])) 
+            return true;
+
+        return false;
     }
 };
