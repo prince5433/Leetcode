b73d0c027ee22dfe6ca7f120b5df7f430795a363
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-03-06 09:46:00 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.47%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-    return s.find("01")==string::npos;
+
+        // find("01") string me "01" pattern search karta hai
+        // agar nahi milta to string::npos return hota hai
+
+        // agar "01" exist karta hai → matlab 1 ka segment khatam ho gaya
+        // aur baad me 1 aa sakta hai → multiple segments possible
+
+        // agar "01" nahi hai → matlab saare 1 ek hi continuous block me hain
+
+        return s.find("01") == string::npos;
     }
 };
