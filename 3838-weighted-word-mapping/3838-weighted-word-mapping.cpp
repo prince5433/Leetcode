e86c2447f544b8b43291071cb958e07050394a34
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-03-06 20:21:48 +0530

    Time: 4 ms (21.28%), Space: 43.8 MB (21.03%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
-        string temp="";
-        int n=words.size();
-        for(string word:words){
-            int sum=0;
-            for(char ch:word){
-                sum+=weights[ch-'a'];
+        
+        string temp = "";  // final result string
+        
+        int n = words.size();  // number of words
+        
+        for(string word : words){  // iterate over each word
+            
+            int sum = 0;  // stores total weight of characters in the word
+            
+            for(char ch : word){  
+                // convert character to index
+                // 'a' -> 0, 'b' -> 1, ..., 'z' -> 25
+                sum += weights[ch - 'a'];
             }
-            int idx=sum%26;
-            temp+=('z'-idx);
+            
+            int idx = sum % 26;  // take modulo to map result within 0–25
+            
+            // reverse alphabet mapping
+            // 0 -> z, 1 -> y, 2 -> x ...
+            temp += ('z' - idx);
         }
-        return temp;
+        
+        return temp;  // return final mapped string
     }
 };
