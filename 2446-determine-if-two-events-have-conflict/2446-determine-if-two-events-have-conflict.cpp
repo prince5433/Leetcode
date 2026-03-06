4395788d46914afcdeb6b53502d1d9d5eedf86be
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-03-06 23:31:39 +0530

    Time: 0 ms (100%), Space: 13.9 MB (22.9%) - LeetHub

---
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool haveConflict(vector<string>& event1, vector<string>& event2) {
+        vector<int> t1,t2;
+        for(string str:event1){
+            string hour=str.substr(0,2);
+            string min=str.substr(3);
+            int hr=stoi(hour);
+            int mi=stoi(min);
+            int time=60*hr+mi;
+            t1.push_back(time);
+        }
+          for(string str:event2){
+            string hour=str.substr(0,2);
+            string min=str.substr(3);
+            int hr=stoi(hour);
+            int mi=stoi(min);
+            int time=60*hr+mi;
+            t2.push_back(time);
+        }
+   if(t1[0] <= t2[1] && t2[0] <= t1[1]) return true;
+        else return false;
+    }
+};
