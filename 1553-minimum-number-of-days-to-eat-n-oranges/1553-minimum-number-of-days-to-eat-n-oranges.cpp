de6077f7cdcc5c89e9fc573dd0aa1d3abdfcbe1d
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-04-13 23:15:39 +0530

    Time: 7 ms (68.54%), Space: 11.6 MB (41.39%) - LeetHub

---
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    unordered_map<int,int> dp;
+
+    int minDays(int n) {
+        if(n <= 1) return n;
+
+        if(dp.count(n)) return dp[n];
+
+        return dp[n] = 1 + min(
+            n % 2 + minDays(n / 2),
+            n % 3 + minDays(n / 3)
+        );
+    }
+};
