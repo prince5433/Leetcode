19972d111b96c7b52005489a55666d1326f4df2c
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-04-17 23:33:08 +0530

    Time: 142 ms (21.96%), Space: 173.8 MB (97.77%) - LeetHub

--- 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
     int countElements(vector<int>& nums, int k) {
-        int cnt=0;
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            int el=nums[i];
-            auto it=upper_bound(nums.begin(),nums.end(),el);
-            int pos=it-nums.begin();
-            int greater=n-pos;
-            if(greater>=k) cnt++;
+        int cnt = 0;
+        int n = nums.size();
+        
+        // 1. Array ko sort kar rahe hain taaki binary search (upper_bound) kaam kar sake
+        // Isse saare chote elements left mein aur bade elements right mein aa jayenge
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 0; i < n; i++) {
+            int el = nums[i];
+            
+            // 2. upper_bound() returns an iterator to the first element 'strictly greater' than 'el'
+            // Hum binary search use kar rahe hain taaki fast pata chale ki bade elements kahan se shuru ho rahe hain
+            auto it = upper_bound(nums.begin(), nums.end(), el);
+            
+            // 3. Iterator ko index mein convert kar rahe hain
+            int pos = it - nums.begin();
+            
+            // 4. 'pos' ke baad wale saare elements 'el' se bade hain.
+            // Toh unka total count hoga (total size - current position index)
+            int greater = n - pos;
+            
+            // 5. Agar strictly greater elements ka count 'k' ya usse zyada hai, 
+            // toh yeh element 'qualified' hai
+            if(greater >= k) {
+                cnt++;
+            }
         }
+        
+        // 6. Total qualified elements return kar rahe hain
         return cnt;
     }
 };
