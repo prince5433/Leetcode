6030804eb8507625022c365213928d79c652f2ee
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-04-23 00:13:54 +0530

    Time: 0 ms (100%), Space: 8.7 MB (54.83%) - LeetHub

--- 3168-minimum-number-of-chairs-in-a-waiting-room/3168-minimum-number-of-chairs-in-a-waiting-room.cpp
+++ 3168-minimum-number-of-chairs-in-a-waiting-room/3168-minimum-number-of-chairs-in-a-waiting-room.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
     int minimumChairs(string s) {
-        int cnt=0;
-        int ans=INT_MIN;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='E') cnt++;
-            else cnt--;
-            ans=max(ans,cnt);
+        
+        // cnt → current number of people inside the room
+        int cnt = 0;
+        
+        // ans → maximum number of people present at any time
+        // this directly represents minimum chairs required
+        int ans = INT_MIN;
+
+        // Traverse the event string
+        for(int i = 0; i < s.size(); i++) {
+            
+            // If person enters → increase count
+            if(s[i] == 'E') 
+                cnt++;
+            else 
+                // If person leaves → decrease count
+                cnt--;
+            
+            // Track maximum people at any moment
+            // because we need chairs for peak occupancy
+            ans = max(ans, cnt);
         }
+
+        // Return the maximum number of people present at once
         return ans;
     }
 };
