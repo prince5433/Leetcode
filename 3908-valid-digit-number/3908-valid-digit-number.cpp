62f1aeb3dee430f4e36ef13a705521dfb540a8ed
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-05-03 00:34:36 +0530

    Time: 0 ms (100%), Space: 9.2 MB (5.23%) - LeetHub

--- 3908-valid-digit-number/3908-valid-digit-number.cpp
+++ 3908-valid-digit-number/3908-valid-digit-number.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     bool validDigit(int n, int x) {
-        string str=to_string(n);
-        string finde=to_string(x);
-        for(int i=0;i<str.length();i++){
-            if(str[0]-'0'==x ) return false;
-            if(str.find(finde)==string::npos) return false;
-        }
+        string str = to_string(n);      // convert number into string
+        string finde = to_string(x);    // convert digit x into string
+
+        // Condition 1:
+        // if first digit itself is x, return false
+        if(str[0] - '0' == x) return false;
+
+        // Condition 2:
+        // if digit x is not present anywhere in number, return false
+        if(str.find(finde) == string::npos) return false;
+
+        // both conditions satisfied
         return true;
     }
 };
