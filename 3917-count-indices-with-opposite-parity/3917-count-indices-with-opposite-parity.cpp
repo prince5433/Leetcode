edad8a9a43eea4d2eb920abfed763b6197808420
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-05-03 20:00:36 +0530

    Time: 4 ms (21.86%), Space: 32.6 MB (96.11%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.cpp
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     vector<int> countOppositeParity(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> ans(n,0);
-        for(int i=0;i<n;i++){
-            int cnt=0;
-            for(int j=i+1;j<n;j++){
-                if(nums[i]%2==0){
-                    if(nums[j]%2==1) cnt++;
+        int n = nums.size();
+
+        // answer array where ans[i] = count of opposite parity elements after i
+        vector<int> ans(n, 0);
+
+        // traverse each element one by one
+        for(int i = 0; i < n; i++) {
+
+            int cnt = 0;   // count for current index i
+
+            // check all elements to the right of i
+            for(int j = i + 1; j < n; j++) {
+
+                // if current element is even
+                if(nums[i] % 2 == 0) {
+
+                    // count odd elements on right side
+                    if(nums[j] % 2 == 1) cnt++;
+
                 } else {
-                    if(nums[j]%2==0) cnt++;
+
+                    // if current element is odd,
+                    // count even elements on right side
+                    if(nums[j] % 2 == 0) cnt++;
                 }
             }
-            ans[i]=cnt;
+
+            // store result for current index
+            ans[i] = cnt;
         }
+
         return ans;
     }
 };
