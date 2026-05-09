7f630018574421517403061fb5f8befa6d96f673
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-05-10 01:31:34 +0530

    Time: 15 ms (68.83%), Space: 13.4 MB (30.43%) - LeetHub

--- 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
@@ -1,30 +1,71 @@
 class Solution {
 public:
-bool isValid(int i,int j,int n){
-    if(i<n && j<n && i>=0 && j>=0) return true;
-    return false;
-}
-    vector<int> executeInstructions(int n, vector<int>& startPos, string s) {
-        int m=s.size();
+
+    // function to check whether current position
+    // is inside the grid or not
+    bool isValid(int i, int j, int n) {
+
+        if (i >= 0 && j >= 0 && i < n && j < n)
+            return true;
+
+        return false;
+    }
+
+    vector<int> executeInstructions(int n,
+                                    vector<int>& startPos,
+                                    string s) {
+
+        int m = s.size();
+
+        // stores answer for every suffix
         vector<int> ans;
-       
-       for(int k=0;k<m;k++){
-        int i=startPos[0];
-        int j=startPos[1];
-         int steps=0;
-         for(int idx=k;idx<m;idx++){
-            char ch=s[idx];
-            if(ch=='R')j++;
-            else if(ch=='L')j--;
-            else if(ch=='U')i--;
-            else i++;
-            if(!isValid(i,j,n)){
-                break;
+
+        // Intuition:
+        // For every starting index k,
+        // start robot again from startPos
+        // and simulate all instructions from k -> end
+
+        for (int k = 0; k < m; k++) {
+
+            // initial robot position
+            int i = startPos[0];
+            int j = startPos[1];
+
+            // counts valid moves
+            int steps = 0;
+
+            // execute instructions from current suffix
+            for (int idx = k; idx < m; idx++) {
+
+                char ch = s[idx];
+
+                // move robot according to instruction
+                if (ch == 'R')
+                    j++;
+
+                else if (ch == 'L')
+                    j--;
+
+                else if (ch == 'U')
+                    i--;
+
+                else
+                    i++;
+
+                // if robot goes outside grid
+                // stop execution
+                if (!isValid(i, j, n)) {
+                    break;
+                }
+
+                // valid move completed
+                steps++;
             }
-            steps++;
-         }
-         ans.push_back(steps);
-       }
-       return ans;
+
+            // store answer for current suffix
+            ans.push_back(steps);
+        }
+
+        return ans;
     }
 };
