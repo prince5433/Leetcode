f62c8cc29a7d7c8d977cc8365e7a0d63dd1cf87a
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-05-26 11:59:33 +0530

    Time: 0 ms (100%), Space: 9.1 MB (68.68%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,18 +1,59 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        vector<int> lower(26,0),upper(26,0);
-        for(char ch:word){
-            if(islower(ch)){
-                lower[ch-'a']=1;
-            } else {
-                upper[ch-'A']=1;
+        
+        /*
+        Intuition:
+        Special char = same letter
+        lowercase + uppercase dono present.
+
+        Example:
+        "aaAbBc"
+
+        a + A -> yes
+        b + B -> yes
+        c only lowercase -> no
+        */
+
+        vector<int> lower(26, 0), upper(26, 0);
+
+        // Mark lowercase / uppercase presence
+        for (char ch : word) {
+            if (islower(ch)) {
+                lower[ch - 'a'] = 1;
+            } 
+            else {
+                upper[ch - 'A'] = 1;
             }
         }
-        int cnt=0;
-        for(int i=0;i<26;i++){
-            if(lower[i]==1 && upper[i]==1) cnt++;
+
+        int cnt = 0;
+
+        // Check common letters
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] == 1 && upper[i] == 1)
+                cnt++;
         }
+
         return cnt;
     }
 };
+
+/*
+Quick Idea:
+2 arrays:
+lower[i] -> lowercase exists
+upper[i] -> uppercase exists
+
+Dono true -> special char.
+
+TC:
+O(n + 26) = O(n)
+
+SC:
+O(26 + 26) = O(1)
+
+Pattern:
+Hashing / Frequency Tracking
+Character Mapping
+*/
