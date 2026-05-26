d13d05b1d0caa9c45d228a3b4c0e9dafdf81f235
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-05-26 12:45:20 +0530

    Time: 1 ms (21.37%), Space: 8.7 MB (30.65%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.cpp
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -1,19 +1,66 @@
 class Solution {
 public:
     string capitalizeTitle(string title) {
+        
+        /*
+        Intuition:
+        Har word ko process karna hai.
+
+        Rule:
+        size <= 2  -> pura lowercase
+        size > 2   -> first uppercase, rest lowercase
+
+        stringstream se words split kar rahe.
+        */
+
         stringstream ss(title);
         string temp;
-        string ans="";
-        while(ss>>temp){
-            for( char& ch:temp){
-                ch=tolower(ch);
+        string ans = "";
+
+        // Traverse each word
+        while (ss >> temp) {
+
+            // Pehle pura lowercase
+            for (char& ch : temp) {
+                ch = tolower(ch);
             }
-            if(temp.size()>2){
-                temp[0]=toupper(temp[0]);
+
+            /*
+            Agar word bada hai
+            to first letter capitalize
+            */
+            if (temp.size() > 2) {
+                temp[0] = toupper(temp[0]);
             }
-            ans+=temp+ " ";
+
+            // Build final string
+            ans += temp + " ";
         }
+
+        // Last extra space remove
         ans.pop_back();
+
         return ans;
     }
 };
+
+/*
+Quick Idea:
+Split words -> lowercase all ->
+if size > 2 capitalize first.
+
+Example:
+"capiTalIze tHe titLe"
+
+"Capitalize The Title"
+
+TC: O(n)
+(each char once)
+
+SC: O(n)
+(answer + stringstream)
+
+Pattern:
+String Processing
+Simulation
+*/
