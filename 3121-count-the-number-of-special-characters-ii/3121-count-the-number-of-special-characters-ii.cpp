96ccad4952846f92e21fd8636ef5a3bbd2606b6e
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-05-27 12:19:15 +0530

    Time: 76 ms (43.54%), Space: 24.5 MB (53.09%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,22 +1,74 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        vector<int> lastLower(26,-1);
-        vector<int> firstUpper(26,-1);
-        for(int i=0;i<word.size();i++){
-            char ch=word[i];
-            if(islower(ch)){
-                lastLower[ch-'a']=i;
-            } else {
-                if(firstUpper[ch-'A']==-1){
-                    firstUpper[ch-'A']=i;
+        
+        /*
+        Intuition:
+        Har character ke liye 2 cheeze track kar rahe:
+        1) last lowercase occurrence
+        2) first uppercase occurrence
+
+        Valid special char tab hoga jab
+        saare lowercase pehle aaye ho
+        => lastLower < firstUpper
+        */
+
+        vector<int> lastLower(26, -1);
+        vector<int> firstUpper(26, -1);
+
+        // Traverse string
+        for (int i = 0; i < word.size(); i++) {
+            char ch = word[i];
+
+            // Update latest lowercase index
+            if (islower(ch)) {
+                lastLower[ch - 'a'] = i;
+            } 
+            else {
+
+                /*
+                Sirf first uppercase store karna hai,
+                baad wale ignore.
+                */
+                if (firstUpper[ch - 'A'] == -1) {
+                    firstUpper[ch - 'A'] = i;
                 }
             }
         }
-        int cnt=0;
-        for(int i=0;i<26;i++){
-            if(lastLower[i]!=-1 && firstUpper[i]!=-1 && lastLower[i]<firstUpper[i]) cnt++;
+
+        int cnt = 0;
+
+        // Check valid characters
+        for (int i = 0; i < 26; i++) {
+            if (lastLower[i] != -1 &&
+                firstUpper[i] != -1 &&
+                lastLower[i] < firstUpper[i]) {
+                cnt++;
+            }
         }
+
         return cnt;
     }
 };
+
+/*
+Quick Idea:
+Last lowercase < First uppercase
+=> valid special char
+
+Example:
+"aaAbcBC"
+
+a -> 1 < 2
+b -> 3 < 5
+c -> 4 < 6
+
+Ans = 3
+
+TC: O(n)
+SC: O(26) = O(1)
+
+Pattern:
+Index Tracking
+Character Mapping
+*/
