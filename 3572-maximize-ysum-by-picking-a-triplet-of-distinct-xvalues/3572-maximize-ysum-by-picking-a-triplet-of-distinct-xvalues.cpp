f7316e136b7d5f7337273b33a87c963e50f16eec
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-05-28 23:11:41 +0530

    Time: 341 ms (5.47%), Space: 235.1 MB (5.97%) - LeetHub

--- 3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.cpp
+++ 3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3572-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.cpp
@@ -1,18 +1,98 @@
 class Solution {
 public:
     int maxSumDistinctTriplet(vector<int>& x, vector<int>& y) {
-        map<int,vector<int>> mp;
-        for(int i=0;i<x.size();i++){
+
+        /*
+        Intuition:
+        Hume 3 distinct x values choose karne hain
+        such that corresponding y sum maximum ho.
+
+        Same x ke liye sirf maximum y useful hoga.
+        */
+
+        map<int, vector<int>> mp;
+
+        // Group all y values by x
+        for(int i = 0; i < x.size(); i++) {
             mp[x[i]].push_back(y[i]);
         }
+
         vector<int> temp;
-        for(auto &it:mp){
-           auto& v=it.second;
-            sort(v.rbegin(),v.rend());
+
+        // For every distinct x
+        for(auto &it : mp) {
+
+            auto& v = it.second;
+
+            /*
+            Largest y chahiye for current x
+            */
+            sort(v.rbegin(), v.rend());
+
+            // store best y for this x
             temp.push_back(v[0]);
         }
-        if(temp.size()<3) return -1;
-        sort(temp.rbegin(),temp.rend());
-        return temp[0]+temp[1]+temp[2];
+
+        /*
+        3 distinct x possible nahi
+        */
+        if(temp.size() < 3)
+            return -1;
+
+        // Need top 3 maximum y values
+        sort(temp.rbegin(), temp.rend());
+
+        return temp[0] + temp[1] + temp[2];
     }
 };
+
+/*
+---------------- QUICK INTUITION ----------------
+
+Example:
+
+x = [1,1,2,2,3]
+y = [5,2,8,1,7]
+
+Group:
+1 -> [5,2]
+2 -> [8,1]
+3 -> [7]
+
+Best pick:
+1 -> 5
+2 -> 8
+3 -> 7
+
+Answer:
+5 + 8 + 7 = 20
+
+------------------------------------------------
+
+Why this works?
+
+Same x multiple baar aa sakta.
+But hume distinct x chahiye.
+
+So har x ke liye sirf max y store karo.
+
+Then top 3 choose.
+
+------------------------------------------------
+
+TC:
+Grouping -> O(n)
+
+Sorting groups + temp:
+Overall approx O(n log n)
+
+SC:
+O(n)
+
+------------------------------------------------
+
+Pattern:
+Hashing / Mapping
+Greedy Selection
+Sorting
+*/
