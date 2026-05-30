836410fc333211ff171779ca82e112e97dcab00b
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-05-31 00:51:45 +0530

    Time: 669 ms (14.85%), Space: 366.4 MB (42.57%) - LeetHub

--- 3161-block-placement-queries/3161-block-placement-queries.cpp
+++ 3161-block-placement-queries/3161-block-placement-queries.cpp
@@ -1,62 +1,77 @@
 class Solution {
 public:
-    vector<int> seg;
-    void update(int idx, int val, int p, int l, int r) {
-        if (l == r) {
-            seg[p] = val;
-            return;
-        }
-        int mid = (l + r) >> 1;
-        if (idx <= mid) {
-            update(idx, val, p << 1, l, mid);
-        } else {
-            update(idx, val, p << 1 | 1, mid + 1, r);
-        }
-        seg[p] = max(seg[p << 1], seg[p << 1 | 1]);
+    vector<int> segmentTree;
+    int n = 50000;
+
+    void constructSegmentTree() {
+        segmentTree.resize(4*n, 0);
     }
 
-    int query(int L, int R, int p, int l, int r) {
-        if (L <= l && r <= R) {
-            return seg[p];
+    void updateSegTree(int idx, int val, int i, int l, int r) {
+        if(l == r) {
+            segmentTree[i] = val;
+            return;
         }
-        int mid = (l + r) >> 1;
-        int res = 0;
-        if (L <= mid) {
-            res = max(res, query(L, R, p << 1, l, mid));
+
+        int mid = l + (r-l)/2;
+
+        if(idx <= mid) {
+            updateSegTree(idx, val, 2*i+1, l, mid);
+        } else {
+            updateSegTree(idx, val, 2*i+2, mid+1, r);
         }
-        if (R > mid) {
-            res = max(res, query(L, R, p << 1 | 1, mid + 1, r));
+
+        segmentTree[i] = max(segmentTree[2*i+1], segmentTree[2*i+2]);
+    }
+
+    int querySegTree(int start, int end, int i, int l, int r) {
+        if(l > end || r < start) { //out of range node hai ye, return kar jao
+            return 0;
         }
-        return res;
+
+        if(l >= start && r <= end) {
+            return segmentTree[i];
+        }
+
+        int mid = l + (r-l)/2;
+
+        return max(querySegTree(start, end, 2*i+1, l, mid), querySegTree(start, end, 2*i+2, mid+1, r));
     }
 
     vector<bool> getResults(vector<vector<int>>& queries) {
-        int mx = 50000;
-        seg.resize(mx << 2);
-        set<int> st = {0, mx};
-        update(mx, mx, 1, 0, mx);
-        vector<bool> ans;
+        constructSegmentTree();
 
-        for (auto& q : queries) {
-            if (q[0] == 1) {
-                int x = q[1];
+        set<int> st; //obstacles
+        st.insert(0);
+
+        vector<bool> result;
+
+        for(auto& query : queries) {
+            if(query[0] == 1) {
+                int x = query[1];
+
                 auto it = st.upper_bound(x);
-                int r = *it;
-                int l = *prev(it);
-                update(x, x - l, 1, 0, mx);
-                update(r, r - x, 1, 0, mx);
+                int nxt = (it != st.end()) ? *it : -1;
+                int pre = *prev(it);
+
+                updateSegTree(x, x-pre, 0, 0, n-1);
+                if(nxt != -1) updateSegTree(nxt, nxt-x, 0, 0, n-1);
+
                 st.insert(x);
             } else {
-                int x = q[1];
-                int sz = q[2];
+                int x = query[1];
+                int sz = query[2];
+
                 auto it = st.upper_bound(x);
-                --it;
-                int pre = *it;
-                int max_space = query(0, pre, 1, 0, mx);
-                max_space = max(max_space, x - pre);
-                ans.push_back(max_space >= sz);
+                int pre = *prev(it);
+
+                int maxGap = querySegTree(0, pre, 0, 0, n-1);
+                int best = max(maxGap, x-pre);
+
+                result.push_back(best >= sz);
             }
         }
-        return ans;
+
+        return result;
     }
 };
