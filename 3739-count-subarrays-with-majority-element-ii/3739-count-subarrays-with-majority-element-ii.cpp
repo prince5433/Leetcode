4da64d0c336afbf5e7ff74d3048b823deaf18a0f
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-06-26 19:20:15 +0530

    Time: 72 ms (55.63%), Space: 114.9 MB (48.34%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        int cumsum=0;
+        mp[0]=1;
+        long long result=0;
+        long long validleftpoints=0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[j]==target){
+                validleftpoints+=mp[cumsum];
+                cumsum+=1;
+            } else{
+                cumsum-=1;
+                validleftpoints-=mp[cumsum];
+            }
+            mp[cumsum]+=1;
+            result+=validleftpoints;
+        }
+        return result;
+    }
+};
