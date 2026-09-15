8820cbf71279861eb0a296ae99bb383efe20f658
Prince Seth <156539518+prince5433@users.noreply.github.com>
2026-09-15 19:31:12 +0530

    Time: 683 ms (5.07%), Space: 40.8 MB (99.25%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,44 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-
 class Solution {
 public:
+    bool exists(TreeNode* root , TreeNode* target){
+        if(root == NULL) return false;
+        if(root == target) return true;
+        return (exists(root->left,target) || exists(root->right,target));
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
-        //mik solutino
-        // 🔹 Base Case 1:
-        // Agar current node NULL hai → kuch nahi mila
-        if(root == NULL) return NULL;
-
-        // 🔹 Base Case 2:
-        // Agar current node hi p ya q hai → wahi answer ho sakta hai
-        // (kyunki LCA wo node bhi ho sakta hai jo khud p ya q ho)
         if(root == p || root == q) return root;
-
-        // 🔹 Recursively left subtree me search karo
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-
-        // 🔹 Recursively right subtree me search karo
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-
-        // 🔹 Case 1:
-        // Agar p aur q dono alag-alag side me mile
-        // → current node hi unka Lowest Common Ancestor hai
-        if(left != NULL && right != NULL) return root;
-
-        // 🔹 Case 2:
-        // Agar sirf left side me mila → answer left subtree me hai
-        if(left != NULL) return left;
-
-        // 🔹 Case 3:
-        // Agar sirf right side me mila → answer right subtree me hai
-        return right;
+        else if(exists(root->left , p) && exists(root->right , q)) return root;
+        else if(exists(root->left , q) && exists(root->right , p)) return root;
+        else if(exists(root->left , p) && exists(root->left , q)) return lowestCommonAncestor(root->left , p , q); //go to a lower ancestor in left
+        else  return lowestCommonAncestor(root->right , p , q); // both exists at right
+        
     }
 };
